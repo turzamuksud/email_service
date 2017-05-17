39fd94d6ef62ac29dab2828d476dcd5e25743dbc
Md. Turza Ibna Muksud <turzamuksud@ymail.com>
2017-05-17 17:16:21 +0600

    for the more option

---
+++ more.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<form>
+<ul class="form-style-1">
+    <li><label>Group Name: </label><input type="text" name="field1" class="field-divided" placeholder="Group" />
+    <li>
+        <label>Email Subject: </label>
+        <input type="text" name="field1" class="field-divided" placeholder="Subject" />
+    </li>
+    
+    <li>
+        <h3>Your Message</h3>
+        <textarea name="field5" id="field5" class="field-long field-textarea" placeholder="Email body"></textarea>
+    </li>
+    
+        <input id="submit" type="submit" value="Send">
+    
+</ul>
+</form>
+
+</body>
+</html>
