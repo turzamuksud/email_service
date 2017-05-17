a8ba92bff1fe4b7fbaec3ec9164bf6d8528a7c04
Md. Turza Ibna Muksud <turzamuksud@ymail.com>
2017-05-17 17:15:40 +0600

    show tables/group records

---
+++ show_table.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM group_list";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Group Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+echo "<a href=\"index.html\"><br><br><br>Go Back</a>";
+$conn->close();
+?>
